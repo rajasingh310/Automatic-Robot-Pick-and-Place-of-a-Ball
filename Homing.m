56c2295ed2489742713449906a9245ea3996af1f
Raj Singh <99217732+rajasingh310@users.noreply.github.com>
2023-10-03 00:48:25 +0200

    Update Homing.m

--- Homing.m
+++ Homing.m
@@ -1,47 +1,52 @@
 %{
 IMP Note: Run only, Pick_Place_Behaviour file.
 
-Kindly change Gripper direction of rotation if robot gets stuck
-at Home
-
 Description: The file contains Homing of Robot and opening of Gripper
 Assumptions:
-1: Motor rotation is not beyond Touch sensor
-2: Gripper is in closed condition and having negative speed for closing
-
+1: Motor rotation is not beyond the Touch sensor
+2: The Gripper is initially in a closed condition with a negative speed for closing
 %}
+
+% Initialize the EV3 robot
 mylego = legoev3('usb');
 
-ts_1 = touchSensor(mylego,1);
-ts_3 = touchSensor(mylego,3);
+% Configure touch sensors for detecting limits
+ts_1 = touchSensor(mylego, 1); % Touch sensor at position 1
+ts_3 = touchSensor(mylego, 3); % Touch sensor at position 3
 
-m_A = motor(mylego, 'A');
-m_B = motor(mylego, 'B');
-m_C = motor(mylego, 'C');
+% Initialize motors
+m_A = motor(mylego, 'A'); % Motor A
+m_B = motor(mylego, 'B'); % Motor B
+m_C = motor(mylego, 'C'); % Motor C
 
+% Start all motors
 start(m_A);
 start(m_B);
 start(m_C);
-m_A.Speed = -40; % reset Gripper
 
-% Homing
+% Set initial speed to reset Gripper
+m_A.Speed = -40;
+
+% Homing procedure to bring the robot to a known starting position
 rts_1 = readTouch(ts_1);
 while rts_1 ~= 1
     rts_3 = readTouch(ts_3);
     while rts_3 ~= 1
-        m_B.Speed = -40; % up motion
+        m_B.Speed = -40; % Move motor B upwards
         rts_3 = readTouch(ts_3);
     end
-    m_B.Speed = 0;
-    m_C.Speed = 30; % left motion
+    m_B.Speed = 0; % Stop motor B
+    m_C.Speed = 30; % Move motor C to the left
     rts_1 = readTouch(ts_1);
 end
-m_C.Speed = 0;
+
+m_C.Speed = 0; % Stop motor C
 pause(0.5);
 
-resetRotation(m_A); % reseting all motors
+% Reset rotation values for all motors
+resetRotation(m_A);
 resetRotation(m_B);
 resetRotation(m_C);
 
-rem = 0; % remainder as a variable
-Gripper % calling function Gripper
+rem = 0; % Initialize remainder as a variable
+Gripper % Call the Gripper function
