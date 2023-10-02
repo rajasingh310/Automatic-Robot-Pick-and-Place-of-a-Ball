491f8629ac23d3359a5b433da2f57cde1e422bd5
Raj Singh <99217732+rajasingh310@users.noreply.github.com>
2023-10-03 00:47:31 +0200

    Update Gripper.m

--- Gripper.m
+++ Gripper.m
@@ -1,5 +1,4 @@
 %{
-
 IMP Note: Run only, Pick_Place_Behaviour file. 
 
 Description: The file contains Gripper to Hold and Release the Ball
@@ -7,15 +6,21 @@ In total no. of positions, at all odd positions Gripper will Hold
 and at all even positions Gripper wil Release
 
 %}
+
+% Pause briefly before starting to allow time for setup
 pause(0.5);
+
+% Check if the remainder of some variable (possibly position index) is 1 (odd)
 if rem == 1
-    while readRotation(m_A) >= 0 % Gripper Hold loop
-        m_A.Speed = -40; % Providing speed to motor A
+    % Gripper Hold loop
+    while readRotation(m_A) >= 0
+        m_A.Speed = -40; % Set the speed of motor A to -40 (to close the gripper)
     end
-    m_A.Speed = 0;
+    m_A.Speed = 0; % Stop motor A when the gripper is closed
 else
-    while readRotation(m_A) <= 80 % Gripper Release loop
-        m_A.Speed = 30; % Providing speed to motor A
+    % Gripper Release loop
+    while readRotation(m_A) <= 80
+        m_A.Speed = 30; % Set the speed of motor A to 30 (to open the gripper)
     end
-    m_A.Speed = 0;
+    m_A.Speed = 0; % Stop motor A when the gripper is open
 end
